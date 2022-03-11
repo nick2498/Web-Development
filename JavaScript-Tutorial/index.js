107c0f66d3e999d6890cc21c5aea3781d2ad8440
Naitik Gupta <guptanaitik98@gmail.com>
2022-03-11 20:57:50 +0530

    Array methods in JS

---
+++ JavaScript-Tutorial/index.js
@@ -0,0 +1,17 @@
+// Array Methods
+/*
+let myArr = ["Fan", "Camera", 34, null, true];
+console.log(myArr.length);
+myArr.pop();
+console.log(myArr);
+myArr.push("Harry");
+console.log(myArr);
+myArr.shift();
+console.log(myArr);
+myArr.unshift("Nick");
+console.log(myArr);
+console.log(myArr.unshift(true));
+console.log(myArr);
+console.log(myArr.toString());
+console.log(myArr.sort());
+*/
